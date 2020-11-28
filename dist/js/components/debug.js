190656d720a00a20e6addbdf1bc3e4791935f309
Oliver Charlet <oliver.charlet@clarities.de>
2020-11-28 14:09:25 +0100

    make debug output switchable

--- dist/js/components/debug.js
+++ dist/js/components/debug.js
@@ -1,7 +1,15 @@
 Vue.component('GuiDebug', {
   mixins: [GuiMixin],
+  data: function() {
+    return {
+      visible: false
+    }
+  },
   template:
 `
-<pre>{{getModel()}}</pre>
+<div>
+  <md-switch v-model="visible">Model</md-switch>
+  <pre v-if="visible">{{getModel()}}</pre>
+</div>
 `
 });
