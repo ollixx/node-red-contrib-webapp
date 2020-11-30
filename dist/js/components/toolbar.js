c96d5ef320cdb14cef9f3c68a37317cd01235222
Oliver Charlet <oliver.charlet@clarities.de>
2020-11-30 17:12:40 +0100

    fix toolbar component

--- dist/js/components/toolbar.js
+++ dist/js/components/toolbar.js
@@ -2,8 +2,8 @@ Vue.component('GuiToolbar', {
   mixins: [GuiMixin],
   template:
     `
-<md-app-toolbar :class="classes" :id="model.nodeid">
+<md-toolbar :class="classes" :id="model.nodeid">
   <span class="md-title">{{ model.payload }}</span>
-</md-app-toolbar>
+</md-toolbar>
 `
 });
