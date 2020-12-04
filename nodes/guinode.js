04465a0ed9793a19ec6800a527bff42745065f08
Oliver Charlet <oliver.charlet@clarities.de>
2020-12-04 18:50:23 +0100

    fix timing of initialization flow

--- nodes/guinode.js
+++ nodes/guinode.js
@@ -5,13 +5,13 @@ winston.remove(winston.transports.Console);
 const logger = winston.createLogger({
     level: "info",
     transports: [
-      new winston.transports.Console(),
+        new winston.transports.Console(),
     ],
     format: winston.format.combine(
         winston.format.splat(),
         winston.format.simple()
     ),
-  });
+});
 
 
 module.exports = function (RED) {
@@ -36,10 +36,15 @@ module.exports = function (RED) {
                 node.wss = node.webapp.wss;
             }
             node.guitype = config.guitype;
-            node.initialized = false;
             node.implicitSetCommand = config.implicitSetCommand;
 
-            logger.debug("props", config.props);
+            // select the Init Messages from my children. I wait with my Init Message until all are collected
+            // key: nodeid, value: {node, InitCommand}
+            node.initialChildCue = false;
+            // cue incoming message at startup until I am initialized
+            node.initialMessageCue = [];
+            // When I have sent my Init Message, I am done and initialized=true 
+            node.initialized = false;
 
             // this nodes gui model
             node.model = {
@@ -60,7 +65,7 @@ module.exports = function (RED) {
                             let value = config.props[prop.name];
                             let type = config.props["$" + prop.name];
                             if (["mod", "par", "pay"].indexOf(type) == -1) {
-                                node.model[prop.name] = RED.util.evaluateNodeProperty(value, type, node, {}); 
+                                node.model[prop.name] = RED.util.evaluateNodeProperty(value, type, node, {});
                             } else {
                                 node.model[prop.name] = value;
                                 node.model["$" + prop.name] = type;
@@ -71,7 +76,7 @@ module.exports = function (RED) {
                     });
                 }
             }
-            
+
             node.createInitMsg = function () {
                 let msg = RED.util.cloneMessage(node.model);
                 msg.command = "Init";
@@ -95,50 +100,26 @@ module.exports = function (RED) {
                 status("red", msg);
             };
 
-            node.on("input", function (msg) {
-
-                // handle incoming commands
-                if (msg.hasOwnProperty("command") || node.implicitSetCommand) {
-
-                    // implicit Set command
-                    if (!msg.command) msg.command = "Set";
-
-                    // handle init event from child
-                    if (msg.command == "Init") {
-                        if (msg.nodeid) {
-
-                            // Keep child in my children
-                            let child = RED.util.cloneMessage(msg)
-                            delete child.command
-                            delete child._msgid
-                            // delete old version of the same child node by id
-                            for (let index = 0; index < node.model.children.length; index++) {
-                                const element = node.model.children[index];
-                                if (element.nodeid == child.nodeid) {
-                                    node.model.children.splice(index, 1);
-                                }
-                            } 
-                            node.model.children.push(child);
-                            node.model.children.sort((a, b) => {
-                                return a._pos - b._pos;
+            let readWiredChildren = function () {
+                if (node.initialChildCue == false) {
+                    node.initialChildCue = {};
+                    RED.nodes.eachNode((child) => {
+                        if (child.wires) {
+                            child.wires.forEach((wires) => {
+                                wires.forEach((nodeid) => {
+                                    if (nodeid == node.id && child.type == "guinode") {
+                                        node.initialChildCue[child.id] = { node: child };
+                                    }
+                                })
                             });
-              
-                            if (node.initialized) {
-                                // This node has already sent Init to parent, so we have to inform our parent of the change
-                                // the final model is save also in the root node, i.e. webapp node
-                                node.send(node.createInitMsg())
-                                //logger.debug("%s - got Init from child. Send again to parent", node.id)
-                            } else {
-                                // This node is not yet initialized, so we store any faster children
-                                // logger.debug("got Init from child. store until my Init")
-                            }
-                        } else {
-                            throw "cannot handle Init command from a non gui-node node"
                         }
-                        return
-                    }
+                    });
+                }
+            }
 
-                    if (msg.command == "Set") {
+            let handleNonInitMessage = function (msg) {
+                switch (msg.command) {
+                    case "Set": {
                         // replace properties from message
                         let type = types.find(t => {
                             return t.name == node.guitype;
@@ -150,11 +131,14 @@ module.exports = function (RED) {
                                 }
                             });
                         }
+                        if (msg.hasOwnProperty("hide")) {
+                            node.model.hide = msg.hide;
+                        }
                         msg = RED.util.cloneMessage(node.model);
                         msg.command = "Set";
+                        break;
                     }
-
-                    if (msg.command == "Add") {
+                    case "Add": {
                         let child = RED.util.cloneMessage(msg)
                         delete child.command
                         delete child._msgid
@@ -163,7 +147,7 @@ module.exports = function (RED) {
                             // no nodeid given, so this node is the target for this command
                             let index = node.model.children.length;
                             // if index exists in children, increase until we got a new one
-                            while (node.model.children.find(ch => {ch.nodeid.endsWith("." + index)})) {index++};
+                            while (node.model.children.find(ch => { ch.nodeid.endsWith("." + index) })) { index++ };
                             msg.nodeid = node.id + "." + index;
                         }
                         node.model.children.push(child);
@@ -171,19 +155,81 @@ module.exports = function (RED) {
                             return a._pos - b._pos;
                         });
                         msg = RED.util.cloneMessage(node.model);
-                        msg.command = command;
+                        msg.command = "Add";
+                        break;
+                    }
+                }
+
+                if (!msg.hasOwnProperty("nodeid")) {
+                    // no nodeid given, so this node is the target for this command
+                    msg.nodeid = node.id
+                }
+
+                //console.log(node.id, "send message to ws", msg);
+                node.webapp.sendWsMessage(msg);
+            };
+
+            node.on("input", function (msg) {
+
+                // console.log(node.id, "got message", msg);
+
+                // handle incoming commands
+                if (msg.hasOwnProperty("command") || node.implicitSetCommand) {
+
+                    // implicit Set command
+                    if (!msg.command) msg.command = "Set";
+
+                    // handle init event from child
+                    if (msg.command == "Init") {
+                        if (!msg.nodeid) {
+                            throw "cannot handle Init command from a non gui-node node"
+                        }
+                        if (node.initialized) {
+                            throw "GuiNode got an Init Message from a child, but is already initialized. That should never happen!"
+                        }
+
+                        // find nodes, that are wired to me, thus are children and keep the list
+                        readWiredChildren();
+
+                        let childData = node.initialChildCue[msg.nodeid];
+                        if (!childData) {
+                            throw "Go an Init Message from an unknown child. That should never happen"
+                        }
+                        if (childData.initMessage) {
+                            throw "Go a second Init Message from a child. That should never happen"
+                        }
+
+                        childData.initMessage = msg;
+                        // console.log(node.id, " got message.", node.initialChildCue)
+
+                        // Keep child in my children
+                        let childModel = RED.util.cloneMessage(msg)
+                        delete childModel.command
+                        delete childModel._msgid
+                        // delete old version of the same child node by id
+                        for (let index = 0; index < node.model.children.length; index++) {
+                            const element = node.model.children[index];
+                            if (element.nodeid == childModel.nodeid) {
+                                node.model.children.splice(index, 1);
+                            }
+                        }
+                        node.model.children.push(childModel);
+                        node.model.children.sort((a, b) => {
+                            return a._pos - b._pos;
+                        });
+
+                        return
                     }
 
-                    if (!msg.hasOwnProperty("nodeid")) {
-                        // no nodeid given, so this node is the target for this command
-                        msg.nodeid = node.id
-                    } else {
-                        msg.parentNodeid = node.id
+                    // here we have a non-init command
+                    if (!node.initialized) {
+                        // we are not initialized, so we cue any messages, that come too early
+                        node.initialMessageCue.push(msg);
+                        return // stop processing and wait for initialization
                     }
 
-                    // TODO: check, if we better send the node.model here, not the msg
-                    //       to be more consistent
-                    node.webapp.sendWsMessage(msg);
+                    handleNonInitMessage(msg)
+
                 } else {
                     statusYellow("received message without a command");
                 }
@@ -195,11 +241,44 @@ module.exports = function (RED) {
                 node.status({});
             });
 
-            setTimeout(function () {
-                node.send(node.createInitMsg())
-                node.initialized = true;
-                statusGreen("sent init msg");
-            }, 100)
+            node.status({});
+
+            let initialize = function() {
+                // are we ready to send? All children should have sent an Init Message
+                readWiredChildren();
+
+                let canInit = true;
+                for (let cd in node.initialChildCue) {
+                    let childData = node.initialChildCue[cd];
+                    if (canInit) {
+                        // check only, if not already found an empty child data entry (not yet received msg from it)
+                        if (!childData.initMessage) {
+                            setTimeout(initialize, 100);
+                            //console.log(node.id, "have not got all children's messages. continue to wait for ", childData.node.id);
+                            canInit = false;
+                        }
+                    }
+                };
+
+                if (canInit) {
+                    // we got Init Messages from all wired children here
+
+                    // send Init Message to parent
+                    node.send(node.createInitMsg())
+                    node.initialized = true;
+    
+                    // TODO: Here we may need to wait until the Init Process is completely done!!!
+
+                    // send any cued messages to the client(s)
+                    node.initialMessageCue.forEach((message) => {
+                        handleNonInitMessage(message);
+                    });
+                    //console.log("initialized");
+                    statusGreen("sent init msg");
+                }
+            }
+            // start waiting for initialization
+            setTimeout(initialize, 100);
 
         } catch (err) {
             console.log("error in create gui node", err)
