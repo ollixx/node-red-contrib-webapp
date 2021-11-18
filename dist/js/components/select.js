b66f4cea39f683c496d64788a299dc4a91859532
Oliver Charlet <oliver.charlet@clarities.de>
2021-11-18 23:20:20 +0100

    refine select component

--- dist/js/components/select.js
+++ dist/js/components/select.js
@@ -1,32 +1,50 @@
 Vue.component('GuiSelect', {
   mixins: [GuiMixin],
   computed: {
-    payload: function() {
+    payload: function () {
       return this.resolve("payload", this);
     },
-    label: function() {
+    label: function () {
       return this.resolve("label", this);
     },
-    options: function() {
+    options: function () {
       return this.resolve("options", this);
     },
-    binding: function() {
+    binding: function () {
       return {
         mod: this.model,
         par: this.parentContext,
         pay: this.payload
       }
+    },
+    current: function () {
+      let cur = this.binding[this.model.$payload][this.model.payload];
+      return cur;
     }
   },
   mounted() {
     // console.log("parentContext", this.model.type, this.model.nodeid, this.parentContext);
   },
+  watch: {
+    "current": function (newValue, oldValue) {
+      if (newValue != oldValue && oldValue !== undefined) {
+        console.log("changed from", oldValue, "to", newValue)
+        this.sendMessage({
+          command: "Change",
+          nodeid: this.model.nodeid,
+          payload: {
+            option: Array.isArray(this.options) ? this.options.find(option => option.value == this.current) : undefined
+          }
+        });
+      }
+    }
+  },
   template:
-`
+    `
 <md-field>
   <label :for="id">{{label}}</label>
   <md-select v-model="binding[model.$payload][model.payload]" :id="id" :md-dense="model.dense" :multiple="model.multiple">
-    <md-option :value="option.value" v-for="option in options" :disabled="option.disabled">{{option.label}}</md-option>
+      <md-option v-for="option in options" :key="option.value" v-if="!option.htmlLabel" :value="option.value" :id="option.value" :disabled="option.disabled"> {{option.label}} </md-option>
   </md-select>
 </md-field>
 `
